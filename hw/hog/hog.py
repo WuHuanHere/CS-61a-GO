c7917c8a63f5ae88bfd453422060d92f4d8bca67
WuHuanHere <1261095579@qq.com>
2023-12-20 15:49:12 +0800

    hog 03

--- hw/hog/hog.py
+++ hw/hog/hog.py
@@ -64,6 +64,10 @@ def take_turn(num_rolls, player_score, opponent_score, dice=six_sided):
     assert num_rolls <= 10, 'Cannot roll more than 10 dice.'
     # BEGIN PROBLEM 3
     "*** YOUR CODE HERE ***"
+    if num_rolls == 0:
+        return boar_brawl(player_score, opponent_score)
+    else:
+        return roll_dice(num_rolls, dice)
     # END PROBLEM 3
 
 
