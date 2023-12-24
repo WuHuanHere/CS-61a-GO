2fe84c2fa9965dc5e02bffd8b6111f30235dd015
WuHuanHere <1261095579@qq.com>
2023-12-24 19:49:59 +0800

    hw01 and Lecture6 LearningRecoding

--- hw/hw01/hw01.py
+++ hw/hw01/hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = lambda x, y: x - y
     else:
-        f = _____
+        f = lambda x, y: x + y
     return f(a, b)
 
 def a_plus_abs_b_syntax_check():
@@ -42,7 +42,7 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return min(i ** 2 + j ** 2, i ** 2 + k ** 2, j ** 2 + k ** 2)
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -66,7 +66,11 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
-
+    max_num = 1
+    for i in range(2, n):
+        if n % i == 0:
+            max_num= max(max_num, i)
+    return max_num
 
 def hailstone(n):
     """Print the hailstone sequence starting at n and return its
@@ -88,4 +92,14 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    num = 1
+    print(n)
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n * 3 + 1
+        print(n)
+        num += 1
+    return num
 
